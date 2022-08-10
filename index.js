00fa85f01c85af54ece231062875f37b9621e5ad
Robin Schmidt <Robin Schmidt>
2022-08-10 21:45:04 +0200

    added deleteListElement() with hardcoded testbuttons

--- index.js
+++ index.js
@@ -9,14 +9,25 @@ const programmStart = () => {
     const addbutton = document.getElementById("submit");
 
     addbutton.addEventListener("click", createListElement);
+
+    const deleteButtons = document.getElementsByClassName("delete-button");
+    const buttonArr = Array.from(deleteButtons);  // Umwandlung zum Array um Iteration zu ermöglichen
+    buttonArr.forEach(button => button.addEventListener('click', removeListElement));
 }
 
-const createListElement = () => { 
+const createListElement = () => {
     console.log(1);
 
     const newLi = document.createElement("li");
     newLi.innerText = document.forms[0].elements["123"].value;
     toDoList.appendChild(newLi);
+};
+
+const removeListElement = (event) => {
+    console.log(event.path);        //gives back an array of elements from current (icon:0) up to root
+    //Alternativen  (jenachdem was syntactisch schöner ist^^)
+    //event.path[2].remove();
+    event.target.parentNode.parentNode.remove();    //target: icon, parent: button, parentparent: li
 };
 
 
