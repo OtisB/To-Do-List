a4d59351bb368940697d3371e708070dde2c85d9
Mike Barlozewski <barlozewski.mike@gmail.com>
2022-08-10 17:29:06 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+const toDoList = document.getElementById('listContainer');
+
+
+const programmStart = () => {
+
+    console.log(21);
+
+    const addbutton = document.getElementById("submit");
+
+    addbutton.addEventListener("click", createListElement);
+}
+
+const createListElement = () => { 
+    console.log(1);
+
+    const newLi = document.createElement("li");
+    newLi.innerText = document.forms[0].elements["123"].value;
+    toDoList.appendChild(newLi);
+};
+
+
+window.addEventListener("load", programmStart);
