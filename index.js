c0be503df6be15933a72011bae692b2302145d4f
Robin Schmidt <Robin Schmidt>
2022-08-11 16:11:57 +0200

    added delete Function for dynamic li elements, bugfixes

--- index.js
+++ index.js
@@ -4,38 +4,31 @@ const toDoList = document.getElementById('list-container');
 
 const programmStart = () => {
     document.getElementById("submit").addEventListener("click", createListElement);
-
-    //nur für die hardcodierten listenelemente
-    const deleteButtons = document.getElementsByClassName("delete-button");
-    const buttonArr = Array.from(deleteButtons);  // Umwandlung zum Array um Iteration zu ermöglichen
-    buttonArr.forEach(button => button.addEventListener('click', removeListElement));
 }
 
 const createListElement = (event) => {
     const newLi = document.createElement("li");
-    newLi.innerText = document.forms[0].elements["input123"].value;     //warum keine id='' -getElementById
+    newLi.innerText = document.forms[0].elements["input123"].value;     //warum keine id='' -getElementById?
     toDoList.appendChild(newLi);
 
     newLi.appendChild(createButton('checked-button', 'fa-check'));
     newLi.appendChild(createButton('edit-button', 'fa-pencil'));
-    let newButton = createButton('delete-button', 'fa-trash-can'); //wegen onclick, wenn editListElement existiert -> auslagern
-    newButton.onclick = removeListElement;
+
+    const newButton = createButton('delete-button', 'fa-trash-can'); //wegen onclick, wenn editListElement existiert -> auslagern
+    newButton.addEventListener('click', () => { removeListElement(event, newLi); });
+
     newLi.appendChild(newButton);
 
-    event.preventDefault(); // Wichtig damit das Formular nicht abgeschickt wird
+    event.preventDefault();
 };
 
-// TODO -- buggy: sometimes removes also <ul>
-const removeListElement = (event) => {
-    //console.log(event.path);        //gives back an array of elements from current (icon:0) up to root
-    //Alternativen  (jenachdem was syntactisch schöner ist^^)
-    //event.path[2].remove();
-    event.target.parentNode.parentNode.remove();    //target: icon, parent: button, parentparent: li
+const removeListElement = (event, listElement) => {
+    listElement.remove();
 };
 
 // TODO -- add event assignment
 let createButton = (className, icon) => {
-    let newButton = document.createElement("button");
+    const newButton = document.createElement("button");
     newButton.classList = `${className}`;
     newButton.innerHTML = `<i class="fa-solid ${icon}"></i>`;
     // newButton.onclick = classList === 'delete-button' ? removeListElement : editElement;
